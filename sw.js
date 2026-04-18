a557083206f74602a928edc9b2c9d2ef60f3f325
Esio Junior <102400407+esiojunior89@users.noreply.github.com>
2026-04-18 01:43:58 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pocketmu-cache-v3';
+const CACHE_NAME = 'pocketmu-cache-v4';
 const urlsToCache = [
     './',
     './index.html',
@@ -14,12 +14,34 @@ self.addEventListener('install', e => {
 });
 
 self.addEventListener('activate', e => {
+    // Essa parte garante que caches antigos e travados sejam deletados
+    e.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
     e.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', e => {
+    // Estratégia Network First (Sempre tenta a internet antes do offline)
     e.respondWith(
-        caches.match(e.request)
-        .then(response => response || fetch(e.request))
+        fetch(e.request).then(response => {
+            // Se tem internet, pega a versão mais nova e já atualiza o cache escondido
+            let responseClone = response.clone();
+            caches.open(CACHE_NAME).then(cache => {
+                cache.put(e.request, responseClone);
+            });
+            return response;
+        }).catch(() => {
+            // Se falhou (sem internet), aí sim puxa do cache offline
+            return caches.match(e.request);
+        })
     );
 });
